1dfb8228e9dd6a12d4fbe7ee693a3024a2a70fa4
IshanAcharya <ishanacharya@live.com>
2023-12-11 21:30:34 +1100

    Fixed indextation on main.py file

--- src/main.py
+++ src/main.py
@@ -2,32 +2,31 @@ from expense_tracker import ExpenseTracker
 from colorama import Fore
 import emoji
 
-def main_menu():
+def main():
     print("Welcome to Budget Buddy, helping you spend smart and live smarter!")
 
-expense_tracker = ExpenseTracker()
-is_running = True
+    expense_tracker = ExpenseTracker()
+    is_running = True
 
-while is_running:
-    main_menu()
-    print(f"{Fore.RED}1. Create new Expense Tracker{Fore.RESET}")
-    print(f"{Fore.CYAN}2. Load existing Expense Tracker{Fore.RESET}")
-    print(f"{Fore.GREEN}3. View instructions{Fore.RESET}")
-    print(f"{Fore.YELLOW}4. Exit{Fore.RESET}")
+    while is_running:
+        print(f"{Fore.RED}1. {emoji.emojize(':new:')} Create new Expense Tracker{Fore.RESET}")
+        print(f"{Fore.CYAN}2. Load existing Expense Tracker{Fore.RESET}")
+        print(f"{Fore.GREEN}3. View instructions{Fore.RESET}")
+        print(f"{Fore.YELLOW}4. Exit{Fore.RESET}")
 
-    choice = input("To get started, please enter which option you'd like to choose (1-4):")
+        choice = input("To get started, please enter which option you'd like to choose (1-4):")
 
-    if choice == "1":
-        expense_tracker.create_new_expense_tracker()
-    elif choice == "2":
-        expense_tracker.load_expense_tracker()
-    elif choice == "3":
-        expense_tracker.view_instructions()
-    elif choice == "4":
-        print("Exiting Budget Buddy. Happy saving!")
-        is_running = False
-    else:
-        print("Your input is invalid. Please choose a menu option between 1-4.")
+        if choice == "1":
+            expense_tracker.create_new_expense_tracker()
+        elif choice == "2":
+            expense_tracker.load_expense_tracker()
+        elif choice == "3":
+            expense_tracker.view_instructions()
+        elif choice == "4":
+            print("Exiting Budget Buddy. Happy saving!")
+            is_running = False
+        else:
+            print("Your input is invalid. Please choose a menu option between 1-4.")
 
 if __name__ == "__main__":
     main()
