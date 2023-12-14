bf3c7b98f85d9ed4985ad8cf2eeb2d8f7653f0f0
IshanAcharya <ishanacharya@live.com>
2023-12-14 17:31:42 +1100

    Modified view_budget function code to print income and expense amount in dollar sign ($). Also added replace methods to record_income and record_expense functions to handle use of $ when inputting data.

--- src/expense_tracker.py
+++ src/expense_tracker.py
@@ -106,7 +106,7 @@ class ExpenseTracker:
         print("You are now recording an income.")
 
         income_name = input("Enter the income name:")
-        income_amount = float(input("Enter income amount:").replace(",", ""))
+        income_amount = float(input("Enter income amount:").replace(",", "").replace("$", ""))
         income_category = input("Enter income category:")
         income_date = datetime.date.today()
 
@@ -119,7 +119,7 @@ class ExpenseTracker:
         print("You are now recording an expense.")
 
         expense_name = input("Enter the expense name:")
-        expense_amount = float(input("Enter expense amount:").replace(",", ""))
+        expense_amount = float(input("Enter expense amount:").replace(",", "").replace("$", ""))
         expense_category = input("Enter expense category:")
         expense_date = datetime.date.today()
 
@@ -172,11 +172,11 @@ class ExpenseTracker:
 
         print("\nIncomes:")
         for income in incomes:
-            print(income)
+            print(f"{income.name}: ${income.amount:.2f} ({income.date})")
 
         print("\nExpenses:")
         for expense in expenses:
-            print(expense)
+            print(f"{expense.name}: ${expense.amount:.2f} ({expense.date})")
 
         print("This is the end of your budget summary.")
 
