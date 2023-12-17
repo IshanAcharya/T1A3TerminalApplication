fea4e0b48a5d3cb1c79163b71ddf7dba5d4d17df
IshanAcharya <ishanacharya@live.com>
2023-12-17 15:48:43 +1100

    Added comments on main.py and expense_tracker.py.

--- src/main.py
+++ src/main.py
@@ -1,20 +1,27 @@
+#Import ExpenseTracker class from expense_tracker module
 from expense_tracker import ExpenseTracker
+#Import colorama and emoji packages
 from colorama import Fore
 import emoji
 
 # Main function for application
 def main():
+    #Welcome message
     print(f"{Fore.BLACK}{emoji.emojize(':chart_increasing:')} Welcome to Budget Buddy, helping you spend smart and live smarter!{Fore.RESET}")
 
+    #Create instance of class
     expense_tracker = ExpenseTracker()
+    #Application loop
     is_running = True
 
     while is_running:
+        #Main menu options
         print(f"\n{Fore.GREEN}1. {emoji.emojize(':money_bag:')} Create new Expense Tracker{Fore.RESET}")
         print(f"{Fore.CYAN}2. {emoji.emojize(':open_file_folder:')} Load existing Expense Tracker{Fore.RESET}")
         print(f"{Fore.YELLOW}3. {emoji.emojize(':spiral_notepad:')}  View instructions{Fore.RESET}")
         print(f"{Fore.MAGENTA}4. {emoji.emojize(':door:')} Exit\n{Fore.RESET}")
-
+       
+       #User input to select menu options
         choice = input(f"{Fore.BLACK}{emoji.emojize(':input_numbers:')}To get started, please enter which option you'd like to choose (1-4):{Fore.RESET}")
 
         if choice == "1":
@@ -24,9 +31,11 @@ def main():
         elif choice == "3":
             expense_tracker.view_instructions()
         elif choice == "4":
+            #Exiting application
             print(f"{Fore.YELLOW} {emoji.emojize(':waving_hand:')}Exiting Budget Buddy. Happy saving!{Fore.RESET}")
             is_running = False
         else:
+            #Error handling for invalid input
             print(f"{Fore.RED}{emoji.emojize(':warning:')} Your input is invalid. Please choose a menu option between 1-4.{Fore.RESET}")
 
 if __name__ == "__main__":
